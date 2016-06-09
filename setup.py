762004c60c500baedf56c20be6e3daf43b59f31f
Tommy Ogden <t@ogden.eu>
2016-06-09 22:45:10 +0100

    Add setup.py, MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MaxwellBloch',
+      version='0.1.0',
+      description='A Python package for solving the Maxwell-Bloch equations.',
+      url='http://github.com/tommyogden/maxwellbloch',
+      author='Thomas P Ogden',
+      author_email='t@ogden.eu',
+      license='MIT',
+      packages=['maxwellbloch'],
+      zip_safe=False)
