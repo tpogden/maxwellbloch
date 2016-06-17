dc7a558c0f7105007b59221325bb42b9cbf1e8bb
Tommy Ogden <t@ogden.eu>
2016-06-17 22:22:25 +0100

    Add OBAtom tests for to, from json

--- maxwellbloch/tests/test_ob_atom.py
+++ maxwellbloch/tests/test_ob_atom.py
@@ -11,6 +11,75 @@ from maxwellbloch import ob_atom
 
 class TestInit(unittest.TestCase):
 
+    json_str_02 = ('{'
+                   '  "decays": ['
+                   '    {'
+                   '      "channels": ['
+                   '        ['
+                   '          0,'
+                   '          1'
+                   '        ],'
+                   '        ['
+                   '          2,'
+                   '          1'
+                   '        ]'
+                   '      ],'
+                   '      "rate": 1.0'
+                   '    },'
+                   '    {'
+                   '      "channels": ['
+                   '        ['
+                   '          2,'
+                   '          1'
+                   '        ]'
+                   '      ],'
+                   '      "rate": 2.0'
+                   '    }'
+                   '  ],'
+                   '  "energies": ['
+                   '    1.0,'
+                   '    2.0,'
+                   '    3.0'
+                   '  ],'
+                   '  "fields": ['
+                   '    {'
+                   '      "coupled_levels": ['
+                   '        ['
+                   '         0,'
+                   '          1'
+                   '        ]'
+                   '      ],'
+                   '      "detuning": 1.0,'
+                   '      "detuning_positive": true,'
+                   '      "label": "probe",'
+                   '      "rabi_freq": 0.001,'
+                   '      "rabi_freq_t_args": {},'
+                   '      "rabi_freq_t_func": null'
+                   '    },'
+                   '    {'
+                   '      "coupled_levels": ['
+                   '        ['
+                   '          1,'
+                   '          2'
+                   '        ]'
+                   '      ],'
+                   '      "detuning": 2.0,'
+                   '      "detuning_positive": false,'
+                   '      "label": "coupling",'
+                   '      "rabi_freq": 10.0,'
+                   '      "rabi_freq_t_args": {'
+                   '        "ampl_1": 1.0,'
+                   '        "off_1": 0.7,'
+                   '        "on_1": 0.3'
+                   '      },'
+                   '      "rabi_freq_t_func": "square_1"'
+                   '    }'
+                   '  ],'
+                   '  "num_states": 3'
+                   '}')
+
+    ob_atom_02 = ob_atom.OBAtom.from_json_str(json_str_02)
+
     def test_init_default(self):
         """  Test Default Initialise """ 
 
@@ -20,6 +89,37 @@ class TestInit(unittest.TestCase):
         self.assertEqual(ob_atom_00.energies, [])
         self.assertEqual(ob_atom_00.decays, [])
         self.assertEqual(ob_atom_00.fields, [])
+
+    def test_to_from_json_str(self):
+
+        ob_atom_00 = ob_atom.OBAtom()
+        ob_atom_01 = ob_atom.OBAtom().from_json_str(ob_atom_00.to_json_str())
+
+        self.assertEqual(ob_atom_00.to_json_str(),
+                         ob_atom_01.to_json_str())        
+
+    def test_to_from_json_str_02(self):
+
+        ob_atom_03 = ob_atom.OBAtom().from_json_str(
+            self.ob_atom_02.to_json_str())
+
+        self.assertEqual(self.ob_atom_02.to_json_str(),
+                         ob_atom_03.to_json_str())           
+
+    def test_to_from_json(self):
+
+        import os
+
+        filepath = "test_ob_atom_02.json"
+
+        self.ob_atom_02.to_json(filepath)
+
+        ob_atom_03 = ob_atom.OBAtom().from_json(filepath)
+        os.remove(filepath)
+
+        self.assertEqual(self.ob_atom_02.to_json_str(),
+                         ob_atom_03.to_json_str())
+
 
 def main():
     unittest.main(verbosity=3)
