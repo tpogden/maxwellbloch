8dd8bb69c0c0a0d8c5acdbee2e95b9519198d7f5
Tommy Ogden <t@ogden.eu>
2016-06-18 16:22:38 +0100

    Add H_0 and c_ops to OBAtom

--- maxwellbloch/ob_atom.py
+++ maxwellbloch/ob_atom.py
@@ -14,6 +14,10 @@ class OBAtom(ob_base.OBBase):
 
         self.build_fields(fields)
 
+        self.build_H_0(energies)
+        self.build_c_ops(decays)
+
+
     def __repr__(self):
         return ("Atom(num_states={0}, " +
                 "energies={1}, " +
@@ -31,6 +35,50 @@ class OBAtom(ob_base.OBBase):
         for f in field_dicts:
             self.add_field(f)
         return self.fields
+
+    def build_H_0(self, energies=[]): 
+        """ Takes a list of energies and makes a Bare Hamiltonian with the
+        energies as diagonals.
+
+        Leave the list empty for all zero energies (i.e. if you don't care
+        about absolute energies.)
+
+        Args:
+            energies: list of pre-interaction energies
+
+        Returns:
+            H_0 = [energies[0]             0  ...]
+                  [             0 energies[1] ...]
+                  [           ...         ... ...]
+
+        """
+
+        if energies:
+            H_0 = np.diag(np.array(energies))
+        else:
+            H_0 = np.zeros([self.num_states, self.num_states])
+
+        self.H_0 = qu.Qobj(H_0)
+        return self.H_0
+
+    def build_c_ops(self, decays=[]):
+        """ Takes a list of spontaneous decay rates and makes a list of
+        collapse operators to be passed to the solver.
+
+        Args: 
+            decays: list of dicts representing decays.
+            e.g.
+            [ { "rate": 1.0, "channels": [[0,1]] }
+              { "rate": 2.0, "channels": [[2,1], [3,1]] } ]
+        """
+
+        self.c_ops = []
+
+        for d in decays:
+            r = d["rate"]
+            for c in d["channels"]:
+                self.c_ops.append(np.sqrt(r)*self.sigma(c[0],c[1]))
+        return self.c_ops
 
     def get_json_dict(self):
 
