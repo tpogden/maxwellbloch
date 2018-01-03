a5b7416eed78cb708b1139f1e21d0b193d8a0623
Tommy Ogden <t@ogden.eu>
2018-01-03 21:23:22 +0000

    Add git short hash to unreleased version numbers

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 from setuptools import setup
+import subprocess
+
+# Semantic versioning
+MAJOR = 0
+MINOR = 3
+PATCH = 0
+IS_RELEASED = False
+
+VERSION = '{0}.{1}.{2}'.format(MAJOR, MINOR, PATCH)
+
+def git_short_hash():
+    """ Returns the short hash of the latest git commit as a string. """
+
+    git_str = subprocess.check_output(['git', 'log', '-1',
+        '--format=%h']).decode('UTF-8').strip()
+    return git_str
+
+FULL_VERSION = VERSION
+if not IS_RELEASED:
+    FULL_VERSION += '+' + git_short_hash()
 
 setup(name='MaxwellBloch',
-      version='0.3.0.dev',
+      version=FULL_VERSION,
       description='A Python package for solving the Maxwell-Bloch equations.',
       url='http://github.com/tommyogden/maxwellbloch',
       author='Thomas P Ogden',
