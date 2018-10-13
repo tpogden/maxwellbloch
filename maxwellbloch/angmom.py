e8c6a65c9e06f251458794713a95f6c43e100336
Thomas P. Ogden <t@ogden.eu>
2018-10-13 17:50:27 +0100

    Add calc_clebsch_hf function

--- maxwellbloch/angmom.py
+++ maxwellbloch/angmom.py
@@ -5,6 +5,35 @@ from scipy import floor, sqrt
 from scipy.misc import factorial
 from numpy import arange
 
+def calc_clebsch_hf(J_a, I_a, F_a, mF_a, J_b, I_b, F_b, mF_b, q):
+    """ Clebsch-Gordan coefficient for the hyperfine transition dipole matrix
+        element. 
+
+        Args:
+            J_a, I_a, F_a, mF_a, J_b, I_b, F_b, mF_b: Angular momentum numbers 
+            for coupled hyperfine levels a and b.
+            q (int): Polarisation. Choose from [-1, 0, 1]
+
+        Returns:
+            (double) The Clebsch-Gordan coefficient.
+
+        Notes:
+            - The transition is from level a to level b.
+            - References to equations in Steck are 
+                Daniel A. Steck, 'Rubidium 87 D Line Data' available online at 
+                http://steck.us/alkalidata. (revision 2.1.4, 23 December 2010).
+    """
+
+    coeff_F = ((-1)**(F_b+J_a+1+I_a)*
+              sqrt((2*F_b+1)*(2*J_a+1))*
+              wigner_6j(J_a,J_b,1,F_b,F_a,I_a)) # Steck, Eqn 36
+
+    coeff_hf = ((-1)**(F_b-1+mF_a)*
+               sqrt(2*F_a+1)*
+               wigner_3j(F_b,1,F_a,mF_b,q,-mF_a)) # Steck, Eqn 35
+
+    return coeff_hf*coeff_F
+
 
 def wigner_3j(j1, j2, j3, m1, m2, m3):
     """ Compute the Wigner 3j factor using the Racah formula.
@@ -28,36 +57,35 @@ def wigner_3j(j1, j2, j3, m1, m2, m3):
     # Additional check if the sum of the second row equals zero
     if (m1 + m2 + m3 != 0):
         # print '3j-Symbol unphysical.'
-        return 0
+        return 0.0
 
     if (j1 - m1 != floor(j1 - m1)):
         # print '2*j1 and 2*m1 must have the same parity'
-        return 0
+        return 0.0
 
     if (j2 - m2 != floor(j2 - m2)):
         # print '2*j2 and 2*m2 must have the same parity'
-        return
-        0
+        return 0.0
 
     if (j3 - m3 != floor(j3 - m3)):
         # print '2*j3 and 2*m3 must have the same parity'
-        return 0
+        return 0.0
 
     if (j3 > j1 + j2) | (j3 < abs(j1 - j2)):
         # print 'j3 is out of bounds.'
-        return 0
+        return 0.0
 
     if abs(m1) > j1:
         # print 'm1 is out of bounds.'
-        return 0
+        return 0.0
 
     if abs(m2) > j2:
         # print 'm2 is out of bounds.'
-        return 0
+        return 0.0
 
     if abs(m3) > j3:
         # print 'm3 is out of bounds.'
-        return 0
+        return 0.0
 
     t1 = j2 - m1 - j3
     t2 = j1 + m2 - j3
@@ -116,7 +144,7 @@ def wigner_6j(j1, j2, j3, J1, J2, J3):
         (abs(J1 - J2) > j3) | 
         (J1 + J2 < j3)):
         # print '6j-Symbol is not triangular!'
-        return 0
+        return 0.0
 
     # Check if the sum of the elements of each traid is an integer
     if ((2 * (j1 + j2 + j3) != round(2 * (j1 + j2 + j3))) | 
@@ -152,7 +180,8 @@ def wigner_6j(j1, j2, j3, J1, J2, J3):
         triangle_coeff(j1, J2, J3) * triangle_coeff(J1, j2, J3) * 
         triangle_coeff(J1, J2, j3))
 
-def triangle_coeff(a,b,c):
-    # Calculating the triangle coefficient
+
+def triangle_coeff(a, b, c):
+    """ Calculate the triangle coefficient. """
     return (factorial(a + b - c) * factorial(a - b + c) * factorial(-a + b + c)
             /factorial(a + b + c + 1))
