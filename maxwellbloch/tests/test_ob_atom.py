1b79e4298ed2c6aca772eee19ab6051fc2539970
Thomas P. Ogden <t@ogden.eu>
2019-07-04 10:18:09 +0100

    Add TestBuildHDelta, w/ failing multiple upper

--- maxwellbloch/tests/test_ob_atom.py
+++ maxwellbloch/tests/test_ob_atom.py
@@ -6,81 +6,84 @@ Thomas Ogden <t@ogden.eu>
 """
 
 import os
-
 import unittest
 
-from maxwellbloch import ob_atom
+import numpy as np
+import qutip as qu
+
+from maxwellbloch import ob_atom, field
+
 
 # Absolute path of tests/json directory, so that tests can be called from
 # different directories.
 JSON_DIR = os.path.abspath(os.path.join(__file__, '../', 'json'))
 
 JSON_STR_02 = ('{'
-               '  "decays": ['
-               '    {'
-               '      "channels": ['
-               '        ['
-               '          0,'
-               '          1'
-               '        ],'
-               '        ['
-               '          2,'
-               '          1'
-               '        ]'
-               '      ],'
-               '      "rate": 1.0'
-               '    },'
-               '    {'
-               '      "channels": ['
-               '        ['
-               '          2,'
-               '          1'
-               '        ]'
-               '      ],'
-               '      "rate": 2.0'
-               '    }'
-               '  ],'
-               '  "energies": ['
-               '    1.0,'
-               '    2.0,'
-               '    3.0'
-               '  ],'
-               '  "fields": ['
-               '    {'
-               '      "coupled_levels": ['
-               '        ['
-               '         0,'
-               '          1'
-               '        ]'
-               '      ],'
-               '      "detuning": 1.0,'
-               '      "detuning_positive": true,'
-               '      "label": "probe",'
-               '      "rabi_freq": 0.001,'
-               '      "rabi_freq_t_args": {},'
-               '      "rabi_freq_t_func": null'
-               '    },'
-               '    {'
-               '      "coupled_levels": ['
-               '        ['
-               '          1,'
-               '          2'
-               '        ]'
-               '      ],'
-               '      "detuning": 2.0,'
-               '      "detuning_positive": false,'
-               '      "label": "coupling",'
-               '      "rabi_freq": 10.0,'
-               '      "rabi_freq_t_args": {'
-               '        "ampl": 1.0,'
-               '        "off": 0.7,'
-               '        "on": 0.3'
-               '      },'
-               '      "rabi_freq_t_func": "square"'
-               '    }'
-               '  ],'
-               '  "num_states": 3'
-               '}')
+    '  "decays": ['
+    '    {'
+    '      "channels": ['
+    '        ['
+    '          0,'
+    '          1'
+    '        ],'
+    '        ['
+    '          2,'
+    '          1'
+    '        ]'
+    '      ],'
+    '      "rate": 1.0'
+    '    },'
+    '    {'
+    '      "channels": ['
+    '        ['
+    '          2,'
+    '          1'
+    '        ]'
+    '      ],'
+    '      "rate": 2.0'
+    '    }'
+    '  ],'
+    '  "energies": ['
+    '    1.0,'
+    '    2.0,'
+    '    3.0'
+    '  ],'
+    '  "fields": ['
+    '    {'
+    '      "coupled_levels": ['
+    '        ['
+    '         0,'
+    '          1'
+    '        ]'
+    '      ],'
+    '      "detuning": 1.0,'
+    '      "detuning_positive": true,'
+    '      "label": "probe",'
+    '      "rabi_freq": 0.001,'
+    '      "rabi_freq_t_args": {},'
+    '      "rabi_freq_t_func": null'
+    '    },'
+    '    {'
+    '      "coupled_levels": ['
+    '        ['
+    '          1,'
+    '          2'
+    '        ]'
+    '      ],'
+    '      "detuning": 2.0,'
+    '      "detuning_positive": false,'
+    '      "label": "coupling",'
+    '      "rabi_freq": 10.0,'
+    '      "rabi_freq_t_args": {'
+    '        "ampl": 1.0,'
+    '        "off": 0.7,'
+    '        "on": 0.3'
+    '      },'
+    '      "rabi_freq_t_func": "square"'
+    '    }'
+    '  ],'
+    '  "num_states": 3'
+    '}')
 
 
 class TestInit(unittest.TestCase):
@@ -160,23 +163,79 @@ class TestJSON(unittest.TestCase):
 
         oba = ob_atom.OBAtom().from_json(json_path)
 
-class TestGetFieldSumCoherence(unittest.TestCase):
 
-    @unittest.skip("states_t() is not initialised so this doesn't work.") 
-    def test_default(self):
+class TestBuildHDelta(unittest.TestCase):
 
-        ob_atom_00 = ob_atom.OBAtom()
+    def test_two_level_no_detuning(self):
 
-        with self.assertRaises(IndexError) as context:
-            ob_atom_00.get_field_sum_coherence(0)
+        f_dict = {'coupled_levels': [[0, 1]]}
+        oba = ob_atom.OBAtom(num_states=2, fields=[f_dict])
 
-        self.assertTrue("list index out of range" in str(context.exception))
+        H_Delta_test = qu.Qobj(np.zeros([oba.num_states, oba.num_states]))
+
+        np.testing.assert_array_almost_equal(oba.H_Delta.data.toarray(), 
+            H_Delta_test.data.toarray())
+
+    def test_two_level_detuning(self):
+
+        DETUNING = 10
+        f_dict = {'coupled_levels': [[0, 1]], 'detuning': DETUNING}
+        oba = ob_atom.OBAtom(num_states=2, fields=[f_dict])
+
+        H_Delta_test = qu.Qobj([[0., 0.], [0., -2*np.pi*DETUNING]])
+
+        np.testing.assert_array_almost_equal(oba.H_Delta.data.toarray(), 
+            H_Delta_test.data.toarray())
+
+    def test_two_level_detuning_positive(self):
+
+        DETUNING = 10
+        f_dict = {'coupled_levels': [[0, 1]], 'detuning': DETUNING, 
+            'detuning_positive': False}
+        oba = ob_atom.OBAtom(num_states=2, fields=[f_dict])
+
+        H_Delta_test = qu.Qobj([[0., 0.], [0., 2*np.pi*DETUNING]])
+
+        np.testing.assert_array_almost_equal(oba.H_Delta.data.toarray(), 
+            H_Delta_test.data.toarray())
+
+    def test_single_field_multiple_upper_levels(self):
+
+        DETUNING = 10
+        f_dict = {'coupled_levels': [[0, 2], [1, 2]], 'detuning': DETUNING}
+        oba = ob_atom.OBAtom(num_states=3, fields=[f_dict])
+
+        H_Delta_test = -2*np.pi*DETUNING*oba.sigma(2, 2)
+
+        np.testing.assert_array_almost_equal(oba.H_Delta.data.toarray(), 
+            H_Delta_test.data.toarray())        
+
+        # H_Delta_array = np.zeros(oba.num_states, oba.num_states)
+        # H_Delta_array[2, 2] = -2*np.pi*DETUNING
+
+        # H_Delta_test = qu.Qobj([[0., 0.], [0., 2*np.pi*DETUNING]])
+
+    # TODO Tests:
+    # three-level (different configurations)
+
+
+# class TestGetFieldSumCoherence(unittest.TestCase):
+
+#     @unittest.skip("states_t() is not initialised so this doesn't work.") 
+#     def test_default(self):
+
+#         ob_atom_00 = ob_atom.OBAtom()
+
+#         with self.assertRaises(IndexError) as context:
+#             ob_atom_00.get_field_sum_coherence(0)
+
+#         self.assertTrue("list index out of range" in str(context.exception))
     
-    @unittest.skip("states_t() is not initialised so this doesn't work.")    
-    def test_initial_condition(self):
+#     @unittest.skip("states_t() is not initialised so this doesn't work.")    
+#     def test_initial_condition(self):
 
-        ob_atom_02 = ob_atom.OBAtom.from_json_str(JSON_STR_02)        
+#         ob_atom_02 = ob_atom.OBAtom.from_json_str(JSON_STR_02)        
 
-        self.assertEqual(ob_atom_02.get_field_sum_coherence(0)[0], 0j)
+#         self.assertEqual(ob_atom_02.get_field_sum_coherence(0)[0], 0j)
 
-        self.assertEqual(ob_atom_02.get_field_sum_coherence(1)[0], 0j)
+#         self.assertEqual(ob_atom_02.get_field_sum_coherence(1)[0], 0j)
