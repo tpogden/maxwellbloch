1de1047300f98e918e6c2db50125527c57fc05c9
Thomas P. Ogden <t@ogden.eu>
2019-12-31 17:11:38 +0000

    Add .readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'MaxwellBloch'
@@ -22,8 +21,7 @@ copyright = '2019, Thomas P. Ogden'
 author = 'Thomas P. Ogden'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.5.0'
-
+# release = 'v0.5.0'
 
 # -- General configuration ---------------------------------------------------
 
@@ -45,14 +43,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -62,5 +58,6 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = []
 
 # -- Options for Jupyter Notebooks -------------------------------------------
-
 nbsphinx_timeout = 600
+nbsphinx_allow_errors = False
+nbsphinx_execute = 'always'
